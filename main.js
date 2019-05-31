38afc7e921e758d40a03aee7b9e5f4c649372687
Alex Rumsey <alexrumsey1993@gmail.com>
2019-05-31 14:43:40 -0500

    added new log

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const blogName= document.querySelector(".article__header").textContent="Welcome to the Alex blog.";
 const allList= document.querySelectorAll(".article__header");
+allList[0].classList.add("important");
 allList[1].classList.add("important");
-    console.log(allList[1].classList);
+    console.log(allList);
 const border= document.querySelector(".dashed").style.border="none";
 const footer= document.querySelector(".article__footer").classList.add("goldenrod");
